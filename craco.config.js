bc132d5a91fd009210a33718f7520e1975149a6c
qianfengg <fred_gqf0221@163.com>
2022-08-08 18:53:37 +0800

    项目搭建-启用移动端适配

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const pxToViewport = require("postcss-px-to-viewport");
 
 module.exports = {
   // webpack 配置
@@ -8,6 +9,17 @@ module.exports = {
       // 约定：使用 @ 表示 src 文件所在路径
       "@": path.resolve(__dirname, "src"),
       "@scss": path.resolve(__dirname, "src/assets/styles"),
+    },
+  },
+  // react-scripts 4.0 的写法：
+  style: {
+    postcss: {
+      plugins: [
+        pxToViewport({
+          // 视口宽度，就是基准尺寸，一般使用 375px（设计师的设计稿一般也按该宽度来设计）
+          viewportWidth: 375,
+        }),
+      ],
     },
   },
 };
