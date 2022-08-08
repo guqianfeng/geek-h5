e2c1fedff6ead71698fba590e28088a9901e52dd
qianfengg <fred_gqf0221@163.com>
2022-08-08 15:31:38 +0800

    项目搭建-设置默认首页

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import "./App.scss";
 import Layout from "./pages/Layout";
 import Login from "./pages/Login";
@@ -8,6 +8,16 @@ export default function App() {
   return (
     <div className="app">
       <h1>我是App</h1>
+      {/* <Route path="/" exact> */}
+      {/* <Redirect to={"/home"}></Redirect> */}
+      {/* </Route>  */}
+      <Route
+        path={"/"}
+        exact
+        render={() => {
+          return <Redirect to={"/home"}></Redirect>;
+        }}
+      ></Route>
       <Route path={"/home"} component={Layout}></Route>
       <Route path={"/login"} component={Login}></Route>
     </div>
