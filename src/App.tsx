cde3cc51296d7de172b5290779bc62f2efc1c8e4
qianfengg <fred_gqf0221@163.com>
2022-08-08 15:09:08 +0800

    路由配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import "./App.scss";
+import Layout from "./pages/Layout";
+import Login from "./pages/Login";
 
 export default function App() {
-  return <div className="app">App</div>;
+  return (
+    <div className="app">
+      <h1>我是App</h1>
+      <Route path={"/home"} component={Layout}></Route>
+      <Route path={"/login"} component={Login}></Route>
+    </div>
+  );
 }
