f44cddccc6d9af75dcdb9f5be9b24ee0a4cd9ad9
qianfengg <fred_gqf0221@163.com>
2022-08-08 19:28:28 +0800

    项目搭建-字体图标

--- src/pages/Playground/index.tsx
+++ src/pages/Playground/index.tsx
@@ -6,6 +6,10 @@ export default function Playground() {
     <div>
       <div className="test-div"></div>
       <div className="test-div"></div>
+      <svg className="icon" aria-hidden="true">
+        {/* 使用时只需要将此处 iconbtn_like_sel 替换为你要的 icon 名称即可 */}
+        <use xlinkHref="#iconbtn_like_sel"></use>
+      </svg>
     </div>
   );
 }
