dafa2dfa022c77f3d5a4f360a4f0276c02d205bc
qianfengg <fred_gqf0221@163.com>
2022-08-08 20:31:01 +0800

    登录页面-表单校验

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -13,7 +13,18 @@ export default function Login() {
         <h2 className="title">账号登录</h2>
 
         <Form>
-          <Form.Item className="login-item">
+          <Form.Item
+            className="login-item"
+            name="mobile"
+            rules={[
+              { required: true, message: "请输入用户名" },
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: "请输入正确的手机号",
+              },
+            ]}
+            validateTrigger={["onChange", "onBlur"]}
+          >
             <Input placeholder="请输入用户名"></Input>
           </Form.Item>
 
@@ -21,13 +32,32 @@ export default function Login() {
             className="login-code-extra"
             extra={<span className="code-extra">发送验证码</span>}
           >
-            <Form.Item className="login-item">
+            <Form.Item
+              className="login-item"
+              name="code"
+              rules={[
+                {
+                  required: true,
+                  message: "请输入验证码",
+                },
+                {
+                  pattern: /\d{6}/,
+                  message: "验证码格式错误",
+                },
+              ]}
+              validateTrigger={["onChange", "onBlur"]}
+            >
               <Input placeholder="请输入验证码"></Input>
             </Form.Item>
           </List.Item>
 
           <Form.Item>
-            <Button color="primary" className="login-submit" block>
+            <Button
+              color="primary"
+              className="login-submit"
+              block
+              type="submit"
+            >
               登录
             </Button>
           </Form.Item>
