cf7684f12650d1ecd0b794cab15a19f04b2fb547
qianfengg <fred_gqf0221@163.com>
2022-08-09 17:09:00 +0800

    13-编写 Layout 页的底部标签栏

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -1,5 +1,36 @@
-import React from "react";
+import Icon from "@/components/Icon";
+import { TabBar } from "antd-mobile";
 
-export default function Layout() {
-  return <div>Layout</div>;
-}
+// 标签页数据
+const tabs = [
+  { path: "/home", icon: "iconbtn_home", text: "首页" },
+  { path: "/home/question", icon: "iconbtn_qa", text: "问答" },
+  { path: "/home/video", icon: "iconbtn_video", text: "视频" },
+  { path: "/home/profile", icon: "iconbtn_mine", text: "我的" },
+];
+
+//<Icon name={item.icon + "_sel"} className="tab-bar-item-icon" />
+const Layout = () => {
+  return (
+    <div>
+      <TabBar className="tab-bar">
+        {tabs.map((item) => (
+          <TabBar.Item
+            key={item.path}
+            title={item.text}
+            icon={(active: boolean) => {
+              return (
+                <Icon
+                  name={item.icon + (active ? "_sel" : "")}
+                  className="tab-bar-item-icon"
+                />
+              );
+            }}
+          />
+        ))}
+      </TabBar>
+    </div>
+  );
+};
+
+export default Layout;
