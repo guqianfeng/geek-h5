2b129de28c2ede935bfbefb4fc5ad9b54cd9278a
qianfengg <fred_gqf0221@163.com>
2022-08-11 11:58:03 +0800

    6-编写顶部静态频道 Tabs

--- src/pages/Layout/Home/index.tsx
+++ src/pages/Layout/Home/index.tsx
@@ -1,18 +1,40 @@
-import Icon from '@/components/Icon'
+import Icon from "@/components/Icon";
+import { Tabs } from "antd-mobile";
 
-import styles from './index.module.scss'
+import styles from "./index.module.scss";
 
 const Home = () => {
   return (
     <div className={styles.root}>
       {/* 频道 Tabs 列表 */}
+      <Tabs className="tabs" activeLineMode="fixed">
+        <Tabs.Tab title="推荐" key="1">
+          推荐频道的内容
+        </Tabs.Tab>
+
+        <Tabs.Tab title="html" key="2">
+          html频道的内容
+        </Tabs.Tab>
+
+        <Tabs.Tab title="开发者资讯" key="3">
+          开发者资讯频道的内容
+        </Tabs.Tab>
+
+        <Tabs.Tab title="c++" key="4">
+          c++频道的内容
+        </Tabs.Tab>
+
+        <Tabs.Tab title="css" key="5">
+          css频道的内容
+        </Tabs.Tab>
+      </Tabs>
 
       <div className="tabs-opration">
         <Icon type="iconbtn_search" />
         <Icon type="iconbtn_channel" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
