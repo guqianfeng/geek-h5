068cc3c474d242744d8687b779d9778cfdf0c9c8
qianfengg <fred_gqf0221@163.com>
2022-08-12 15:15:29 +0800

    图片懒加载原理讲解

---
+++ src/pages/Playground/1-测试Icon.tsx
@@ -0,0 +1,24 @@
+import Icon from "@/components/Icon";
+import React from "react";
+
+import styles from "./index.module.css";
+// console.log(styles);
+export default function Playground() {
+  return (
+    <div>
+      <div className={styles["test-div"]}></div>
+      <div className="test-div"></div>
+      <svg className="icon" aria-hidden="true">
+        {/* 使用时只需要将此处 iconbtn_like_sel 替换为你要的 icon 名称即可 */}
+        <use xlinkHref="#iconbtn_like_sel"></use>
+      </svg>
+      <Icon
+        className="abc"
+        type="iconbtn_like_sel"
+        onClick={() => {
+          console.log("click");
+        }}
+      />
+    </div>
+  );
+}
--- src/pages/Playground/index.tsx
+++ src/pages/Playground/index.tsx
@@ -1,24 +1,31 @@
-import Icon from "@/components/Icon";
-import React from "react";
-
+import { useEffect, useRef } from "react";
 import styles from "./index.module.css";
 // console.log(styles);
+// http://geek.itheima.net/resources/images/19.jpg
 export default function Playground() {
+  const imgRef = useRef<HTMLImageElement>(null);
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const entry = entries[0];
+      console.log(entry.isIntersecting);
+      if (entry.isIntersecting) {
+        const imgEl = entry.target as HTMLImageElement;
+        imgEl.src = imgEl.dataset["src"]!;
+        observer.unobserve(imgEl);
+      }
+    });
+    observer.observe(imgRef.current!);
+  }, []);
   return (
-    <div>
-      <div className={styles["test-div"]}></div>
-      <div className="test-div"></div>
-      <svg className="icon" aria-hidden="true">
-        {/* 使用时只需要将此处 iconbtn_like_sel 替换为你要的 icon 名称即可 */}
-        <use xlinkHref="#iconbtn_like_sel"></use>
-      </svg>
-      <Icon
-        className="abc"
-        type="iconbtn_like_sel"
-        onClick={() => {
-          console.log("click");
-        }}
-      />
-    </div>
+    <>
+      <div className={styles["test-img-div"]}>
+        <h1>图片懒加载</h1>
+      </div>
+      <img
+        ref={imgRef}
+        data-src="http://geek.itheima.net/resources/images/19.jpg"
+        alt="test"
+      ></img>
+    </>
   );
 }
