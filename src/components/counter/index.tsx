bc3807684657838c6ca6624d96c3c8bb52abcae5
qianfengg <fred_gqf0221@163.com>
2022-08-16 15:52:21 +0800

    6-React补充知识：shouldComponentUpdate

--- src/components/counter/index.tsx
+++ src/components/counter/index.tsx
@@ -7,9 +7,14 @@ export default class Counter extends Component {
   clickHandler = () => {
     this.setState({
       count: this.state.count + 1,
+      // count: 0,
     });
   };
+  shouldComponentUpdate(nextProps: any, nextState: any) {
+    return this.state.count !== nextState.count;
+  }
   render() {
+    console.log(">>>>>>>>>>>", this.state.count);
     return (
       <div>
         <h1>计数器：{this.state.count}</h1>
